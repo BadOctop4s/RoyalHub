780ee122a4c6a6f9d1fff61c9369992bb693b54a
BadOctop4s <eodraxkk6@gmail.com>
2026-01-23 00:09:36 -0300

    FIX12

--- Source.lua
+++ Source.lua
@@ -402,10 +402,70 @@ if Window then
         Title = "Informações",
         Icon = "info",
     })
+
+    local Window = WindUI:CreateWindow({
+    Title = "",
+    Icon = "door-open", -- lucide icon
+    Author = "by .ftgs and .ftgs",
+    Folder = "MySuperHub",
+    
+    -- ↓ This all is Optional. You can remove it.
+    Size = UDim2.fromOffset(580, 460),
+    MinSize = Vector2.new(560, 350),
+    MaxSize = Vector2.new(850, 560),
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+    
+    -- ↓ Optional. You can remove it.
+    --[[ You can set 'rbxassetid://' or video to Background.
+        'rbxassetid://':
+            Background = "rbxassetid://", -- rbxassetid
+        Video:
+            Background = "video:YOUR-RAW-LINK-TO-VIDEO.webm", -- video 
+    --]]
+    
+    -- ↓ Optional. You can remove it.
+    User = {
+        Enabled = true,
+        Anonymous = true,
+        Callback = function()
+            print("clicked")
+        end,
+    },
+    
+    --       remove this all, 
+    -- !  ↓  if you DON'T need the key system
+    KeySystem = { 
+        -- ↓ Optional. You can remove it.
+        Key = { "1234", "5678" },
+        
+        Note = "Example Key System.",
+        
+        -- ↓ Optional. You can remove it.
+        Thumbnail = {
+            Image = "rbxassetid://",
+            Title = "Thumbnail",
+        },
+        
+        -- ↓ Optional. You can remove it.
+        URL = "YOUR LINK TO GET KEY (Discord, Linkvertise, Pastebin, etc.)",
+        
+        -- ↓ Optional. You can remove it.
+        SaveKey = false, -- automatically save and load the key.
+        
+        -- ↓ Optional. You can remove it.
+        -- API = {} ← Services. Read about it below ↓
+    },
+})
 ---------------------------------------Buttons--------------------------------------
 local Slider = PersonagemTab:Slider({
     Title = "Velocidade"
-    Desc =  "Nada ainda"
+    Desc =  "Nada ainda" 
 
     Step = 1,
     value = {
@@ -414,13 +474,9 @@ local Slider = PersonagemTab:Slider({
         Default = 50,
     },
     Callback = function(value)
-        local function setSpeed(value)
-             local player = S.Players.LocalPlayer
-             local humanoid = player.Character and player.Character:FindFirstChild("Humanoid")
-             if humanoid then
-                 humanoid.WalkSpeed = value
-             end
-        end
+         setSpeed(value)
+    end
+        
 })
     
     local infoSection = InfoTab:CreateSection("Informações do Servidor")
