32561a029ac564c6f1381bf00a06583e974f9053
BadOctop4s <eodraxkk6@gmail.com>
2026-01-23 00:15:08 -0300

    minor

--- Source.lua
+++ Source.lua
@@ -321,6 +321,7 @@ if Window then
                     print("Continuando...")
                 end,
                 Variant = "Primary",
+            end
             }
         }
     })
@@ -402,66 +403,8 @@ if Window then
         Title = "Informações",
         Icon = "info",
     })
-
-    local Window = WindUI:CreateWindow({
-    Title = "Funções",
-    Icon = "Function", -- lucide icon
-
-    Folder = "",
-    
-    -- ↓ This all is Optional. You can remove it.
-    Size = UDim2.fromOffset(580, 460),
-    MinSize = Vector2.new(560, 350),
-    MaxSize = Vector2.new(850, 560),
-    Transparent = true,
-    Theme = "Dark",
-    Resizable = true,
-    SideBarWidth = 200,
-    BackgroundImageTransparency = 0.42,
-    HideSearchBar = true,
-    ScrollBarEnabled = false,
-    
-    -- ↓ Optional. You can remove it.
-    --[[ You can set 'rbxassetid://' or video to Background.
-        'rbxassetid://':
-            Background = "rbxassetid://", -- rbxassetid
-        Video:
-            Background = "video:YOUR-RAW-LINK-TO-VIDEO.webm", -- video 
-    --]]
-    
-    -- ↓ Optional. You can remove it.
-    User = {
-        Enabled = true,
-        Anonymous = true,
-        Callback = function()
-            print("clicked")
-        end,
-    },
-    
-    --       remove this all, 
-    -- !  ↓  if you DON'T need the key system
-    KeySystem = { 
-        -- ↓ Optional. You can remove it.
-        Key = { "1234", "5678" },
-        
-        Note = "Example Key System.",
-        
-        -- ↓ Optional. You can remove it.
-        Thumbnail = {
-            Image = "rbxassetid://",
-            Title = "Thumbnail",
-        },
-        
-        -- ↓ Optional. You can remove it.
-        URL = "YOUR LINK TO GET KEY (Discord, Linkvertise, Pastebin, etc.)",
-        
-        -- ↓ Optional. You can remove it.
-        SaveKey = false, -- automatically save and load the key.
-        
-        -- ↓ Optional. You can remove it.
-        -- API = {} ← Services. Read about it below ↓
-    },
-})
+    local FunçõesTab = Window:Tab({
+-- Title = "Funções
 ---------------------------------------Buttons--------------------------------------
 local Slider = PersonagemTab:Slider({
     Title = "Velocidade",
